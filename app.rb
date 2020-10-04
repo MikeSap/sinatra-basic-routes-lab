2e6ffc22db6c9e06dad901762eb941103e6a9d8f
mike <mikesapienzaaudio@gmail.com>
2020-10-04 18:33:53 -0500

    status not reading

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
-class App < Sinatra::Base
+class App < Sinatra::Base    
+
+
+    get '/name' do
+        status 200
+        "My name is #{name}"
+     
+    end
+
+    get '/hometown' do
+        status 200
+        "My hometown is #{hometown}"
+       
+    end
+
+    get '/favorite-song' do
+        status 200
+        "My favorite song is #{favorite-song}"       
+    end
+
 end
